710c0717e6ce827f3f1dcf8c027d6620d1fa73e3
Phai Rotana <phai.rotana99@gmail.com>
2024-07-13 16:53:06 +0700

    Updated

--- 04_hw_terraform_workspace/provider.tf
+++ 04_hw_terraform_workspace/provider.tf
@@ -1,7 +1,9 @@
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAW3MEDSO3YMMRMN3Z"
-  secret_key = "AoJrjf5dWh2SwMX77DFybf7VMPpEHijqrvMsKbWd"
+  access_key = "Your"
+  secret_key = "Your"
+  # access_key = "AKIAW3MEDSO3YMMRMN3Z"
+  # secret_key = "AoJrjf5dWh2SwMX77DFybf7VMPpEHijqrvMsKbWd"
 }
 
